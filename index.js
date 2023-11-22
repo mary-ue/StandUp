bdda58c742e8229206f46c314f7096178eb79827
mary-ue <maryandreeva100@gmail.com>
2023-11-22 17:04:47 +0300

    create server

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Node.js');
+import http from 'node:http';
+import fs from 'node:fs/promises';
+
+const PORT = 8080;
+
+http 
+  .createServer(async (req, res) => {
+    if (req.method === "GET" && req.url === "/comedians") {
+      try {
+        const data = await fs.readFile('comedians.json', 'utf-8');
+
+        res.writeHead(200, {
+          "Content-type": "text/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*"
+        });
+        res.end(data);
+      } catch (error) {
+        res.writeHead(500, {
+          "Content-type": "text/plain; charset=utf-8",
+        });
+        res.end(`Ошибка сервера: ${error}`);
+      }
+    } else {
+      res.writeHead(404, {
+        "Content-type": "text/plain; charset=utf-8",
+      });
+      res.end('Not found');
+    }
+  })
+  .listen(PORT);
+
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
