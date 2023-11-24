80b8ccf9283545948e4180425a8aefd5c985fe59
mary-ue <maryandreeva100@gmail.com>
2023-11-25 00:22:26 +0300

    add handleClientsRequest

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { sendData, sendError } from './modules/send.js';
 import { checkFile } from './modules/checkFile.js';
 import { handleComediansRequest } from './modules/handleComediansRequest.js';
 import { handleAddClient } from './modules/handleAddClient.js';
+import { handleClientsRequest } from './modules/handleClientsRequest.js';
 
 const PORT = 8080;
 const COMEDIANS = './comedians.json';
@@ -40,8 +41,8 @@ const startServer = async () => {
         }
   
         if (req.method === 'GET' && segments[0] === 'clients' && segments.length === 2) {
-          const ticket = segments[1];
-          handleClientsRequest(req, res, ticket);
+          const ticketNumber = segments[1];
+          handleClientsRequest(req, res, ticketNumber);
           return;
           // GET /clients/:ticket
           // get client from ticket number
